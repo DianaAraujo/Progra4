8e69dd1b71ce80c9b1caae376d8be30e82a3ffa6
Carlos Monge <carcmonge@gmail.com>
2016-09-21 15:42:37 -0600

    [Add] Imagen para favicon y código HTML para este

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta charset="UTF-8">
 	<meta name="author" content="Grupo progra">
-	<!--<meta http-equiv="refresh" content="900">-->
-	<link rel="icon" href="elephant.png">
+
+	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon" />
 
 	<title>¿Quién quiere ser Millonario?</title>
 
